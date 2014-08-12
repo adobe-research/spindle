8937ac0639592c0e86e56e7dbd91cc125f623a7b
Brandon Amos <bamos@cmu.edu>
2014-08-12 15:44:24 -0700

    Add AnalyticsData Thrift schema.

---
+++ src/main/thrift/AnalyticsData.thrift
@@ -0,0 +1,32 @@
+///////////////////////////////////////////////////////////////////////////
+//
+// Copyright (c) 2014 Adobe Systems Incorporated. All rights reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+///////////////////////////////////////////////////////////////////////////
+
+namespace java com.adobe.thrift
+
+struct AnalyticsData {
+  1: string post_pagename;
+  2: string user_agent;
+  3: string visit_referrer;
+  4: string post_visid_high;
+  5: string post_visid_low;
+  6: string visit_num;
+  7: string hit_time_gmt;
+  8: string post_purchaseid;
+  9: string post_product_list;
+  10: string first_hit_referrer;
+}
