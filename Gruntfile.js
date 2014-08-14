f6f243b0da8c4d16647e81a6a6575c56adb5ba27
Brandon Amos <bamos@cmu.edu>
2014-08-14 03:51:29 -0700

    Initial commit of Gruntfile to deploy the demo to github pages.

---
+++ Gruntfile.js
@@ -0,0 +1,20 @@
+module.exports = function(grunt) {
+  grunt.initConfig({
+    buildcontrol: {
+      options: {
+        dir: 'demo',
+        commit: true,
+        push: true,
+        message: 'Built %sourceName% from commit %sourceCommit% on branch %sourceBranch%.'
+      },
+      pages: {
+        options: {
+          remote: 'git@https://git.corp.adobe.com/amos/spindle.git',
+          branch: 'gh-pages'
+        }
+      }
+    }
+  });
+  grunt.loadNpmTasks('grunt-build-control');
+  grunt.registerTask('deploy', ['buildcontrol:pages']);
+}
